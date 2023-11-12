bdaa129d7c884abfbafd9fd62b3915d7b95f125e
nishizawayuya <22aw0128@jec.ac.jp>
2023-11-13 03:18:25 +0900

    最小限のアニメーションの追加

--- content.js
+++ content.js
@@ -4,6 +4,29 @@ window.onload = () => {
   // ボタンの宣言
   const btns = document.querySelectorAll(".chome-btn");
 
+  // フェードインのアニメーション関数
+  function fadeIn(element) {
+    let opacity = 0;
+    let position = -100;
+
+    // 100ミリ秒ごとにopacityを増加させる
+    const intervalId = setInterval(function() {
+      opacity += 0.1;
+      element.style.opacity = opacity;
+
+      // opacityが1以上になったらアニメーションを停止
+      if (opacity >= 1) {
+        clearInterval(intervalId);
+      }
+    }, 100);
+  }
+
+  function removetext(element) {
+    setTimeout(function() {
+      element.remove();
+    }, 1000); //
+  }  
+
   btns.forEach(function(btn,index) {
     btn.addEventListener("click",()=>{
       console.log(btn);
@@ -15,11 +38,23 @@ window.onload = () => {
         // <p>要素にテキストを設定
         newParagraph.textContent = 'すごい';
         // <p>にクラス付与
-        newParagraph.classList.add('animete-text');
+        newParagraph.classList.add('animete-text1');
+
+        //cssの指定
         newParagraph.style.color=("#ff0000");
+        newParagraph.style.position=("absolute");
+        newParagraph.style.right=("10%");
+        newParagraph.style.bottom=("10%");
+
         // <body>要素に新しい<p>要素を追加
         document.body.appendChild(newParagraph);
 
+        // アニメーションを追加
+        fadeIn(newParagraph);
+
+        //pタグを削除する
+        const chrometextdata = document.querySelector(".animete-text1");
+        removetext(chrometextdata);
       }
       // ボタン2
       else if(index == 1){
@@ -29,11 +64,25 @@ window.onload = () => {
         // <p>要素にテキストを設定
         newParagraph.textContent = 'かっこいい';
         // <p>にクラス付与
-        newParagraph.classList.add('animete-text');
+        newParagraph.classList.add('animete-text2');
+
+         //cssの指定
         newParagraph.style.color=("#0000ff");
+        newParagraph.style.position=("absolute");
+        newParagraph.style.right=("10%");
+        newParagraph.style.bottom=("10%");
+
         // <body>要素に新しい<p>要素を追加
         document.body.appendChild(newParagraph);
+
+        // アニメーションを追加
+        fadeIn(newParagraph);
+
+        //pタグを削除する
+        const chrometextdata = document.querySelector(".animete-text2");
+        removetext(chrometextdata);
       }
+
       // ボタン3
       else if(index == 2){
         console.log("btn3 ok");
@@ -42,10 +91,20 @@ window.onload = () => {
         // <p>要素にテキストを設定
         newParagraph.textContent = '可愛い';
         // <p>にクラス付与
-        newParagraph.classList.add('animete-text');
+        newParagraph.classList.add('animete-text3');
+        
+         //cssの指定
         newParagraph.style.color=("#00ff00");
+        newParagraph.style.position=("absolute");
+        newParagraph.style.right=("10%");
+        newParagraph.style.bottom=("10%");
+        
         // <body>要素に新しい<p>要素を追加
         document.body.appendChild(newParagraph);
+
+        //pタグを削除する
+        const chrometextdata = document.querySelector(".animete-text3");
+        removetext(chrometextdata);
       }
     })
   });
